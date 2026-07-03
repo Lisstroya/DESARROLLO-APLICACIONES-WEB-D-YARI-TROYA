a2981b64e36cb034f06d1e3f0b643c7f47756c43
Liss Troya <yl.troyay@uea.edu.ec>
2026-07-03 14:27:05 -0500

    Proyecto Integrador U2 - Actualizacion Semana 6

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const formulario = document.getElementById("formContacto");
 
 const nombre = document.getElementById("nombre");
 const correo = document.getElementById("correo");
+const tipoSolicitud = document.getElementById("tipoSolicitud");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
+const mensajeFormulario = document.getElementById("mensajeFormulario");
 
 const errorNombre = document.getElementById("errorNombre");
 const errorCorreo = document.getElementById("errorCorreo");
+const errorTipo = document.getElementById("errorTipo");
 const errorAsunto = document.getElementById("errorAsunto");
 const errorMensaje = document.getElementById("errorMensaje");
 
@@ -15,47 +18,163 @@ const totalRegistros = document.getElementById("totalRegistros");
 
 let contador = 0;
 
+function validarNombre() {
+
+    if (nombre.value.trim() === "") {
+
+        errorNombre.textContent = "Ingrese su nombre";
+        nombre.classList.add("is-invalid");
+        nombre.classList.remove("is-valid");
+
+        return false;
+    }
+
+    if (nombre.value.trim().length < 3) {
+
+        errorNombre.textContent = "El nombre debe tener al menos 3 caracteres.";
+        nombre.classList.add("is-invalid");
+        nombre.classList.remove("is-valid");
+
+        return false;
+    }
+
+    errorNombre.textContent = "";
+    nombre.classList.remove("is-invalid");
+    nombre.classList.add("is-valid");
+
+    return true;
+
+}
+
+function validarCorreo() {
+
+    if (correo.value.trim() === "") {
+
+        errorCorreo.textContent = "Ingrese su correo electrónico";
+        correo.classList.add("is-invalid");
+        correo.classList.remove("is-valid");
+
+        return false;
+    }
+
+        errorCorreo.textContent = "";
+        correo.classList.remove("is-invalid");
+        correo.classList.add("is-valid");
+
+        return true;
+
+}
+
+function validarTipo() {
+
+    if (tipoSolicitud.value === "") {
+
+        errorTipo.textContent = "Seleccione un tipo de solicitud";
+        tipoSolicitud.classList.add("is-invalid");
+        tipoSolicitud.classList.remove("is-valid");
+
+        return false;
+    }
+
+    errorTipo.textContent = "";
+    tipoSolicitud.classList.remove("is-invalid");
+    tipoSolicitud.classList.add("is-valid");
+
+    return true;
+
+}
+
+function validarAsunto() {
+
+    if (asunto.value.trim() === "") {
+
+        errorAsunto.textContent = "Ingrese un asunto";
+        asunto.classList.add("is-invalid");
+        asunto.classList.remove("is-valid");
+
+        return false;
+    }
+
+    errorAsunto.textContent = "";
+    asunto.classList.remove("is-invalid");
+    asunto.classList.add("is-valid");
+
+    return true;
+
+}
+
+function validarMensaje() {
+
+    if (mensaje.value.trim() === "") {
+
+        errorMensaje.textContent = "Ingrese un mensaje";
+        mensaje.classList.add("is-invalid");
+        mensaje.classList.remove("is-valid");
+
+        return false;
+    }
+
+    if (mensaje.value.trim().length < 15) {
+
+        errorMensaje.textContent = "El mensaje debe tener al menos 15 caracteres.";
+        mensaje.classList.add("is-invalid");
+        mensaje.classList.remove("is-valid");
+
+        return false;
+    }
+
+    errorMensaje.textContent = "";
+    mensaje.classList.remove("is-invalid");
+    mensaje.classList.add("is-valid");
+
+    return true;
+
+}
+
+
 formulario.addEventListener("submit", function (evento) {
 
     evento.preventDefault();
 
     errorNombre.textContent = "";
     errorCorreo.textContent = "";
+    errorTipo.textContent = "";
     errorAsunto.textContent = "";
     errorMensaje.textContent = "";
 
     nombre.classList.remove("is-invalid");
     correo.classList.remove("is-invalid");
+    tipoSolicitud.classList.remove("is-invalid");
     asunto.classList.remove("is-invalid");
     mensaje.classList.remove("is-invalid");
 
     let valido = true;
 
-    if (nombre.value.trim() === "") {
-        errorNombre.textContent = "Ingrese su nombre";
-        nombre.classList.add("is-invalid");
-        valido = false;
+    if (!validarNombre()) {
+    valido = false;
     }
 
-    if (correo.value.trim() === "") {
-        errorCorreo.textContent = "Ingrese su correo electrónico";
-        correo.classList.add("is-invalid");
-        valido = false;
+    if (!validarCorreo()) {
+    valido = false;
     }
 
-    if (asunto.value.trim() === "") {
-        errorAsunto.textContent = "Ingrese un asunto";
-        asunto.classList.add("is-invalid");
-        valido = false;
+    if (!validarTipo()) {
+    valido = false;
     }
 
-    if (mensaje.value.trim() === "") {
-        errorMensaje.textContent = "Ingrese un mensaje";
-        mensaje.classList.add("is-invalid");
-        valido = false;
+    if (!validarAsunto()) {
+    valido = false;
+    }
+    
+    if (!validarMensaje()) {
+    valido = false;
     }
 
     if (!valido) {
+
+        mensajeFormulario.innerHTML =
+        '<div class="alert alert-danger">Corrija los errores antes de enviar el formulario.</div>';
+        
         return;
     }
 
@@ -68,6 +187,9 @@ formulario.addEventListener("submit", function (evento) {
 
     const textoCorreo = document.createElement("p");
     textoCorreo.innerHTML = "<strong>Correo:</strong> " + correo.value;
+
+    const textoTipo = document.createElement("p");
+    textoTipo.innerHTML = "<strong>Tipo de Solicitud:</strong> " + tipoSolicitud.value;
 
     const textoAsunto = document.createElement("p");
     textoAsunto.innerHTML = "<strong>Asunto:</strong> " + asunto.value;
@@ -93,6 +215,7 @@ formulario.addEventListener("submit", function (evento) {
 
     tarjeta.appendChild(textoNombre);
     tarjeta.appendChild(textoCorreo);
+    tarjeta.appendChild(textoTipo);
     tarjeta.appendChild(textoAsunto);
     tarjeta.appendChild(textoMensaje);
     tarjeta.appendChild(botonEliminar);
@@ -103,6 +226,28 @@ formulario.addEventListener("submit", function (evento) {
 
     totalRegistros.textContent = contador;
 
+    mensajeFormulario.innerHTML =
+    '<div class="alert alert-success">Solicitud registrada correctamente.</div>';
+
     formulario.reset();
 
+    nombre.classList.remove("is-valid");
+    correo.classList.remove("is-valid");
+    tipoSolicitud.classList.remove("is-valid");
+    asunto.classList.remove("is-valid");
+    mensaje.classList.remove("is-valid");
+
 });
+
+nombre.addEventListener("input", validarNombre);
+nombre.addEventListener("blur", validarNombre);
+
+correo.addEventListener("input", validarCorreo);
+correo.addEventListener("blur", validarCorreo);
+
+tipoSolicitud.addEventListener("change", validarTipo);
+
+asunto.addEventListener("blur", validarAsunto);
+
+mensaje.addEventListener("input", validarMensaje);
+mensaje.addEventListener("blur", validarMensaje)
